55b9245049768b1093c014f27a7d3b7053b7712a
Cmeesh11 <cartermeesh11@gmail.com>
2023-03-03 23:03:58 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+// TODO: Create an array of questions for user input
+const questions = [
+  "What is the title of your project?",
+  "Write a description for your project.",
+  "What are the steps required to install your project?",
+  "Provide instructions and examples for use: ",
+  "What are your guidelines for future contributers?",
+  "Provide instructions and code for testing (what commands to run, what to click on, etc.)."
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
